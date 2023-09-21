65bac921f50fefac728085f7fa69e1f85bb3f7dd
Chaenahyun <nahyun0105@naver.com>
2023-09-21 12:41:41 +0900

    최종 수정

--- character_grass.py
+++ character_grass.py
@@ -10,20 +10,17 @@ def render_frame(x, y):
     clear_canvas_now()
     grass.draw_now(400, 30)
     character.draw_now(x, y)
-    delay(0.1)
+    delay(0.01)
 
 def run_circle():
     print('CIRCLE')
 
     cx, cy, r = 400, 300, 200
     for deg in range(0, 360, 5):
-        x = r*math.cos(math.radians(deg))
-        y = r*math.sin(math.radians(deg))
+        x = cx+r*math.cos(math.radians(deg))
+        y = cy+r*math.sin(math.radians(deg))
+        render_frame(x, y)
 
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        delay(0.1)
 
 def run_rectangle():
     print('RECTANGLR')
@@ -31,21 +28,18 @@ def run_rectangle():
     #bottom line
     for x in range(50, 750+1, 5):
         render_frame(x, 50)  #x, y위치에 캐릭터 그려줄 수 있는
+  
+    #right line
+    for y in range(50, 550+1, 5):
+        render_frame(750, y)
+
     #top line
     for x in range(750, 50-1, -5):
         render_frame(x, 550)
-    #right line
-    for x in range(50, 550+1, 5):
-        render_frame(750, y)
+        
     #left line
-    for x in range(550, 50-1, -5):
-        render_frame(50, y)
-
-
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        delay(0.1)
+    for y in range(550, 50-1, -5):
+        render_frame(50, y)      
     
 while True:
     run_circle()
