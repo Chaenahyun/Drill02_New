ff2d046514fcfe3897d1d8bc73bfb1f22cab375b
Chaenahyun <nahyun0105@naver.com>
2023-09-21 11:53:48 +0900

    move
    circle, rectangle

--- character_grass.py
+++ character_grass.py
@@ -3,9 +3,18 @@ from pico2d import *
 open_canvas()
 
 # fill here
+grass = load_image('grass.png')
 
 image = load_image('character.png')
 image.draw_now(400, 300)
+delay(1)
 
+def run_circle():
+    print('CIRCLE')
+    pass
+
+def run_rectangle():
+    print('RECTANGLR')
+    pass
 
 close_canvas()
